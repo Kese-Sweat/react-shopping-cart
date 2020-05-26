dd7a89b49e3bb9b3cf2e252c8907bf1007c647a8
Markese Straughter <Kese08@hotmail.com>
2020-05-26 14:12:27 -0700

    added content to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -13,15 +13,34 @@ function Cart () {
     
     
 
-return(<div>
+return(<div className="cartContainer">
     <span className="shoppingCart" >< GiShoppingCart className="shoppingCartImg" size={50}/></span>
-
+<div className="cartContent">
     {cart.map(item => {
-        return<div>
-                    <li>{item.title}</li>
-                    <li>test</li>
+        return<div className="leftSideCart">
+                 <div>
+                     <img className="thumbImg" src={item.img.thumb}></img>
+                    </div>
+                    <div>
+                        <li>{item.title}</li>
+                     <li>{item.style}</li>
+                        <span>Quantity</span>
+                    </div>
                 </div>
     })}
+
+    {cart.map(item => {
+        return<div className="rightSideCart">
+                        <p>X</p>
+                        <p>{item.price}</p>
+                    <div>
+                        <button>-</button>
+                        <button>+</button>
+                    </div>
+
+                </div>
+    })}
+</div>
 
 
 </div>
